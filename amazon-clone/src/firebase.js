3252fbc325a3ac9972d977a1f840d1186a0e8fd5
Gardian <gardianii@hotmail.com>
2020-12-29 23:52:27 +0100

    header component style and other stuff

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9-TQB-YtQo8I3Fc-4R7SNxhMXij9s0TU",
+    authDomain: "clone-88446.firebaseapp.com",
+    projectId: "clone-88446",
+    storageBucket: "clone-88446.appspot.com",
+    messagingSenderId: "311476161332",
+    appId: "1:311476161332:web:ad7e9145da4396a9227361",
+    measurementId: "G-Y8Q2C1L5YQ"
+  };
