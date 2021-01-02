b1b875122eb381fe3994e3db64236023ca2a9d7d
Gardian <gardianii@hotmail.com>
2021-01-02 18:23:55 +0100

    Create login and register page and authentiacation with firebase

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA9-TQB-YtQo8I3Fc-4R7SNxhMXij9s0TU",
@@ -8,3 +10,9 @@ const firebaseConfig = {
     appId: "1:311476161332:web:ad7e9145da4396a9227361",
     measurementId: "G-Y8Q2C1L5YQ"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+
+  export {db, auth}
